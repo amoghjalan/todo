5ac4cce3319f4ae73e4dd28c8a4027856aead8e3
amoghjalan <amoghjalan2005@gmail.com>
2022-07-14 14:42:02 +0530

    Removed title as important in update schema

--- todo-app/components/AddItem.js
+++ todo-app/components/AddItem.js
@@ -1,9 +1,18 @@
 import styles from "../styles/Home.module.css";
 import { useState } from "react";
-import { gql, useQuery, useMutation } from "@apollo/client";
+import { gql, useMutation } from "@apollo/client";
+
+const ADD_TODO = gql``;
 
 const AddItem = ({}) => {
+  const [addTodo, { data, loading, error }] = useMutation(ADD_TODO);
   const [text, settext] = useState("");
+
+  const handleClick = e => {
+    e.preventDefault();
+    addTodo({ variables: { addTitle: text } });
+    e.target.value = "";
+  };
   return (
     <div>
       <input
@@ -11,7 +20,9 @@ const AddItem = ({}) => {
         className={styles.inputItem}
         onChange={e => settext(e.target.value)}
       />
-      <button className={styles.addButton}>Add</button>
+      <button className={styles.addButton} onClick={e => handleClick(e)}>
+        Add
+      </button>
     </div>
   );
 };
--- todo-app/components/ListItem.js
+++ todo-app/components/ListItem.js
@@ -1,17 +1,37 @@
 import React from "react";
 import styles from "../styles/Home.module.css";
+import { gql, useMutation } from "@apollo/client";
+
+const UPDATE_TODO = gql``;
+const DELETE_TODO = gql``;
 
 const ListItem = ({ item }) => {
+  const [updateTodo, { udata, uloading, uerror }] = useMutation(UPDATE_TODO);
+  const [deleteTodo, { ddata, dloading, derror }] = useMutation(DELETE_TODO);
+
+  const handleCheck = () => {
+    updateTodo({ variables: { curId: item.id, status: !item.isCompleted } });
+  };
+  // const handleEdit = () => {
+  //   updateTodo({ variables: { curId: item.id, title:  } });
+  // }
+  const handleDelete = e => {
+    e.preventDefault();
+    deleteTodo({ variables: { curId: item.id } });
+  };
   return (
     <div className={styles.ListItem}>
       <input
         type="checkbox"
         className={styles.checkbox}
         defaultChecked={item.isCompleted}
+        onChange={handleCheck}
       />
       <p className={styles.itemTitle}>{item.title}</p>
-      <button className={styles.editButton}>Edit</button>
-      <button className={styles.deleteButton}>Delete</button>
+      {/* <button className={styles.editButton}>Edit</button> */}
+      <button className={styles.deleteButton} onClick={e => handleDelete(e)}>
+        Delete
+      </button>
     </div>
   );
 };
